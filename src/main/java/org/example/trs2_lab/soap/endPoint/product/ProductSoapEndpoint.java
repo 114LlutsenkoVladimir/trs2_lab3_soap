7976bffa0c7b742c5971f6c00c7892761bc06101
114lutsenkoladimir <lucenkovaldymar@gmail.com>
2026-04-16 13:27:25 +0300

    .

---
+++ src/main/java/org/example/trs2_lab/soap/endPoint/product/ProductSoapEndpoint.java
@@ -0,0 +1,39 @@
+package org.example.trs2_lab.soap.endPoint.product;
+
+import org.example.trs2_lab.entity.Product;
+import org.example.trs2_lab.repository.base.ProductRepository;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import java.util.List;
+
+public class ProductSoapEndpoint {
+    private static final String NAMESPACE_URI = "http://mycompany.com/products";
+
+    private final ProductRepository productRepository;
+
+    // Внедряем ваш существующий репозиторий через конструктор
+    public ProductSoapEndpoint(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetProductsByManufacturerRequest")
+    @ResponsePayload
+    public GetProductsByManufacturerResponse getProducts(@RequestPayload GetProductsByManufacturerRequest request) {
+
+        // Используем ваш обычный метод репозитория!
+        List<Product> entities = productRepository.findByManufacturerId(request.getManufacturerId());
+
+        // Превращаем Entity из базы в XML-объекты для ответа
+        GetProductsByManufacturerResponse response = new GetProductsByManufacturerResponse();
+        for (Product entity : entities) {
+            ProductSoapModel s = new ProductSoapModel();
+            s.setId(entity.getId());
+            s.setName(entity.getName());
+            response.getProducts().add(s);
+        }
+
+        return response;
+    }
+}
